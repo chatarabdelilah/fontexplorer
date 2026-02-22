eb636e432ddca9bc2126f442d78c875c2789dd98
Chatar Abdelilah Salim <chatar.abdelilah.s@gmail.com>
2026-02-22 21:58:34 +0100

    create footer fragment, create head fragment, create navbar fragment, create GlobalControllerAdvice.java, create home.html view, create HomeController.java, style using bootstrap, only necessary styling in style.css

---
+++ src/main/java/be/thomasmore/music/config/GlobalControllerAdvice.java
@@ -0,0 +1,14 @@
+package be.thomasmore.music.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("currentUrl")
+    public String getCurrentUrl(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+}
