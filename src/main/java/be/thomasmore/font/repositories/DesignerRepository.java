5a36937f44950c292d7515ba480ae1e6f14ef524
Chatar Abdelilah Salim <chatar.abdelilah.s@gmail.com>
2026-03-10 22:29:22 +0100

    Create DesignerRepository interface.

---
+++ src/main/java/be/thomasmore/font/repositories/DesignerRepository.java
@@ -0,0 +1,7 @@
+package be.thomasmore.font.repositories;
+
+import be.thomasmore.font.model.Designer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DesignerRepository extends CrudRepository<Designer, Integer> {
+}
