e6118dedead37c05f065764b671516304c7efd6f
Chatar Abdelilah Salim <chatar.abdelilah.s@gmail.com>
2026-03-18 17:10:30 +0100

    Add logger, annotations and initialize repositories for FontAdminController

--- src/main/java/be/thomasmore/font/controllers/admin/FontAdminController.java
+++ src/main/java/be/thomasmore/font/controllers/admin/FontAdminController.java
@@ -1,4 +1,22 @@
 package be.thomasmore.font.controllers.admin;
 
+import be.thomasmore.font.repositories.DesignerRepository;
+import be.thomasmore.font.repositories.FontRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class FontAdminController {
+    private final FontRepository fontRepository;
+    private final DesignerRepository designerRepository;
+
+    private Logger logger = LoggerFactory.getLogger(FontAdminController.class);
+
+    public FontAdminController(FontRepository fontRepository, DesignerRepository designerRepository) {
+        this.fontRepository = fontRepository;
+        this.designerRepository = designerRepository;
+    }
 }
